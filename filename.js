ccfe6d10f7c1ebc304afda087c95d702a50e35ae
Mike Lindle <myklindle@gmail.com>
2021-10-28 16:24:36 -0400

    Adding sections
    lots of changes here, adding sections and basic nav

---
+++ filename.js
@@ -0,0 +1,22 @@
+alert("Select OK to forfeit all likeness rights");
+
+// this is how you comment in JS mike
+
+/* this
+is 
+for a multiline
+comment*/
+
+let myFunction = function loggingFunction (message) { 
+ console.log(message);
+}
+
+myFunction('HELLONURSE');
+
+function getSecretCode(value) {
+let code= value * 42;
+return code;
+
+}
+let secretCode
+console.log( getSecretCode(2) ); 
---
+++ utils.js
@@ -0,0 +1,11 @@
+function showMessage(message) {
+    document .getElementById('message') .textContent = message;
+}
+
+
+
+const button = document.getElementById(origininals);
+
+button.addEventListener('click', function(){
+    console.log('click');
+});
